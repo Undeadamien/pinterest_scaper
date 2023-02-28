c109b1b82a0950b61c9a48bed0c66a13d40f3a17
Undeadamien <126392901+Undeadamien@users.noreply.github.com>
2023-02-28 13:42:18 +0100

    Update pinterest_scraper.py

--- pinterest_scraper.py
+++ pinterest_scraper.py
@@ -4,34 +4,38 @@ from random import choice
 from selenium import webdriver
 from bs4 import BeautifulSoup
 
-SEARCH = ["kan liu", "sam does art", "ross draws", "ilya kushinov", ]
+# where the images should be saved
+IMAGE_PATH = "C:/Users/Damien/Desktop/DRAWING_/Photo_ref"
+SEARCH = ["kan liu", "sam does art", "ross draws", "ilya kushinov"]
 DRIVER = webdriver.Chrome()
 
-key_words = choice(SEARCH)
-key_words = key_words.strip()
-key_words = key_words.replace(" ", "%20")
+# we have to use selenium because the images won't load with request
+DRIVER.get("https://www.pinterest.fr/search/pins/?q="
+           f"{choice(SEARCH).strip().replace(' ', '%20')}"
+           "&rs=typed")
 
-DRIVER.get(f"https://www.pinterest.fr/search/pins/?q={key_words}&rs=typed")
-DRIVER.maximize_window()
-sleep(2)
+DRIVER.maximize_window()  # to load more images on screen
+sleep(3)
 soup = BeautifulSoup(DRIVER.page_source, "html.parser")
 
+# get all the links to all the pins present on screen
 links = []
 for element in soup.find_all("a", href=True):
     if "/pin/" in element["href"]:
         links.append(element["href"])
 
-DRIVER.get(f"https://www.pinterest.fr{choice(links)}")
-soup = BeautifulSoup(DRIVER.page_source, "html.parser")
+# we don't need to load the page with selenium
+page_source = requests.get(f"https://www.pinterest.fr{choice(links)}")
+soup = BeautifulSoup(page_source.content, "html.parser")
 image_url = soup.find("img", src=True)["src"]
 
+image_name = image_url.split("/")[-1]
 
-splited_image_url = image_url.split("/")
-image_name = splited_image_url[-1]
-print(image_name)
-
-if image_name.endswith("jpg"):
-    with open(image_name, "wb") as image_file:
+# save the image if the image is a jpg
+if image_name.endswith(".jpg"):
+    with open(f"{IMAGE_PATH}/{image_name}", "wb") as image_file:
         image_file.write(requests.get(image_url).content)
+else:
+    print("the image was a png")
 
 DRIVER.quit()
